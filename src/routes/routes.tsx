9fd3822172954e1fa617a457edcc388a4b709cb5
Yohannes Getachew <demisgech@gmail.com>
2026-05-08 17:32:59 +0300

    Configure for production

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import type { RouteObject } from "react-router";
 import { RootLayout } from "@/layouts";
-import { HomePage, AboutPage, SkillsPage, ContactPage, ProjectsPage, ServicesPage } from "@/pages";
+import {
+  HomePage,
+  AboutPage,
+  SkillsPage,
+  ContactPage,
+  ProjectsPage,
+  ServicesPage,
+} from "@/pages";
 
 export const routes: RouteObject[] = [
   {
-        element: <RootLayout />,
-        children: [
+    element: <RootLayout />,
+    children: [
       {
         index: true,
         path: "/",
@@ -35,6 +42,5 @@ export const routes: RouteObject[] = [
     ],
   },
 ];
-
 
 export const router = createBrowserRouter(routes);
